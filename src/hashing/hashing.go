38f1edee0aac1dd52612252b773cb0bafa18c8b2
Brooklyn <yuanotes@gmail.com>
2013-03-18 23:14:24 +0800

    finish hashing table and open address

--- src/hashing/hashing.go
+++ src/hashing/hashing.go
@@ -5,32 +5,142 @@ type Element struct {
     next *Element
 }
 
-func Lookup(s string, hash_table []*Element) *Element {
-    index :=Hash(s)
-    if hash_table[index] != nil {
-        ele := hash_table[index]
-        for ; ele.next == nil; ele=ele.next {
-            if ele.value == s {
-                return ele
+// linked list hashtable
+type HashTable struct {
+    table_size int
+    alphabet_size int
+    table []*Element
+}
+
+// open addressing hashtable
+type HashArray struct {
+    table_size int
+    alphabet_size int
+    table []string
+}
+
+func (h *HashArray) Lookup(s string) bool {
+    index := h.Hash(s)
+    if index < 0 || index >= h.table_size {
+        return false
+    }
+    ele := h.table[index]
+    if ele != "" {
+        if ele == s { return true } // found at the hash index
+        original_index := index
+        for {
+            index = (index + 1) % h.table_size
+            if index == original_index { return false } // avoid infinite loop
+            if h.table[index] == s { return true }
+        }
+    }
+    return false
+}
+
+func (h *HashArray) Insert(s string) bool {
+    index := h.Hash(s)
+    if index < 0 || index >= h.table_size {
+        return false
+    }
+    if h.table[index]== "" {
+        h.table[index] = s
+    } else {
+        original_index := index
+        for {
+            index = (index + 1) % h.table_size
+            if index == original_index { return false } // avoid infinite loop
+            if h.table[index] == "" {
+                h.table[index] = s
+                break
             }
         }
-        ele.next = &Element{s, nil}
-    } else {
-        hash_table[index] = &Element{s, nil} 
+    }
+    return true
+}
+
+func (h *HashArray) Hash(s string) int {
+    result := 0
+    for i, v := range s {
+        result += h.alphabet_size^i * int(v)
+    }
+    return result % h.table_size
+}
+
+func (h *HashArray) Delete(s string) bool {
+    index := h.Hash(s)
+    if index < 0 && index >= h.table_size { return false }
+    if h.table[index] == "" { return false }
+    if h.table[index] == s {
+        h.table[index] = "<DELETED>"
+        return true
+    }
+    original_index := index
+    for {
+        index = (index + 1) % h.table_size
+        if index == original_index { return false } // avoid infinite loop
+        if h.table[index] == s {
+            h.table[index] = "<DELETED"
+            return true
+        }
+    }
+    return false
+}
+
+func (h *HashTable) Lookup(s string) *Element {
+    index := h.Hash(s)
+    if index < 0 || index >= h.table_size {
+        return nil
+    }
+    for ele := h.table[index]; ele != nil; ele=ele.next {
+        if ele.value == s {
+            return ele
+        }
     }
     return nil
 }
 
 // Return a number from string.
-func Hash(s string) (int) {
+func (h *HashTable) Hash(s string) (int) {
     result := 0
     for i, v := range s {
-        result += 26^i * int(v)
+        result += h.alphabet_size^i * int(v)
     }
-    return result % 255
+    return result % h.table_size
 }
 
-func Init() []*Element {
-    hash_table := make([]*Element, 255)
-    return hash_table
+func (h *HashTable) Insert(s string) *Element {
+    index := h.Hash(s)
+    if index < 0 || index >= h.table_size {
+        return nil
+    }
+    ele := h.table[index]
+    if ele != nil {
+        for {
+            if ele.next == nil {
+                ele.next = &Element{s, nil}
+                break
+            }
+        }
+        ele = &Element{s, nil}
+    } else {
+        h.table[index] = &Element{s, nil}
+    }
+    return ele
+}
+
+func (h *HashTable) Delete(s string) bool {
+    index := h.Hash(s)
+    if index < 0 && index >= h.table_size { return false }
+    ele := h.table[index]
+    if ele.value == s {
+        h.table[index] = ele.next
+        return true
+    }
+    for ; ele != nil ; ele=ele.next{
+        if ele.value == s {
+            ele = ele.next
+            return true
+        }
+    }
+    return false
 }
--- src/hashing/hashing_test.go
+++ src/hashing/hashing_test.go
@@ -4,37 +4,116 @@ import (
     "testing"
 )
 
+var h = &HashTable{255, 26, make([]*Element, 255)}
+var h2 = &HashArray{255, 26, make([]string,255)}
 
-func TestHash(t *testing.T) {
-    str1 := "hello world"
-    if Hash(str1) == 0 {
-        t.Errorf("Hash function get 0...")
-    }
-}
-
-func TestLookup(t *testing.T) {
+func TestLinkedlistHashTable(t *testing.T) {
     str1 := "hello world"
     str2 := "你好世界"
     str3 := ""
-    hash_table := Init()
-    if Lookup(str1, hash_table) != nil {
+    if h.Hash(str1) == 0 {
+        t.Errorf("Hash function get 0...")
+    }
+    if h.Hash(str2) == 0 {
+        t.Errorf("Hash function get 0...")
+    }
+    if h.Hash(str3) != 0 {
+        t.Errorf("Hash function get 0...")
+    }
+    if h.Lookup(str1) != nil {
         t.Error("The string should be non-exist.")
     }
-    if Lookup(str2, hash_table) != nil {
+    if h.Lookup(str2) != nil {
         t.Error("The string should be non-exist.")
     }
-    if Lookup(str3, hash_table) != nil {
+    if h.Lookup(str3) != nil {
         t.Error("The string should be non-exist.")
     }
-    if Lookup(str1, hash_table) == nil {
-        t.Errorf("The string %v should be exist in %v", str1, hash_table)
+    h.Insert(str1)
+    h.Insert(str2)
+    h.Insert(str3)
+    if h.Lookup(str1) == nil {
+        t.Errorf("The string should exist in table.")
     }
-    if Lookup(str2, hash_table) == nil {
-        t.Errorf("The string %v should be exist in %v", str2, hash_table)
+    if h.Lookup(str2) == nil {
+        t.Errorf("The string should exist in table.")
     }
-    if Lookup(str3, hash_table) == nil {
-        t.Errorf("The string %v should be exist in %v", str3, hash_table)
+    if h.Lookup(str3) == nil {
+        t.Errorf("The string should exist in table.")
     }
-    t.Logf("hash_table is %v", hash_table)
-    
+    h.Insert(str1)
+    h.Delete(str1)
+    h.Delete(str1)
+    h.Delete(str2)
+    h.Delete(str3)
+    if h.Lookup(str1) != nil {
+        t.Error("The string should be exist.")
+    }
+    if h.Lookup(str2) != nil {
+        t.Error("The string should be non-exist.")
+    }
+    if h.Lookup(str3) != nil {
+        t.Error("The string should be non-exist.")
+    }
+}
+
+
+
+func TestHashArray(t *testing.T) {
+    str1 := "hello world"
+    str2 := "你好世界"
+    str3 := "!@#@@#$%#"
+    if h2.Hash(str1) == 0 {
+        t.Errorf("Hash function get 0...")
+    }
+    if h2.Hash(str2) == 0 {
+        t.Errorf("Hash function get 0...")
+    }
+    if h2.Hash(str3) == 0 {
+        t.Errorf("Hash function get 0...")
+    }
+    if h2.Lookup(str1) {
+        t.Error("The string should be non-exist.")
+    }
+    if h2.Lookup(str2) {
+        t.Error("The string should be non-exist.")
+    }
+    if h2.Lookup(str3) {
+        t.Error("The string should be non-exist.")
+    }
+    h2.Insert(str1)
+    h2.Insert(str2)
+    h2.Insert(str3)
+    t.Logf("now hash array is %v", h2.table)
+    if !h2.Lookup(str1) {
+        t.Errorf("The string should exist in table.")
+    }
+    if !h2.Lookup(str2) {
+        t.Errorf("The string should exist in table.")
+    }
+    if !h2.Lookup(str3) {
+        t.Errorf("The string should exist in table.")
+    }
+    for i:=0;i<300;i++{
+        h2.Insert(str1)
+    }
+    t.Log("now hash array is", h2.table)
+    h2.Delete(str1)
+    h2.Delete(str1)
+    for i:=0;i<300;i++{
+        h2.Delete(str1)
+    }
+    t.Log("now hash array is", h2.table)
+    h2.Delete(str2)
+    h2.Delete(str3)
+    t.Log("now hash array is", h2.table)
+    if h2.Lookup(str1) {
+        t.Error("The string should be non-exist.")
+    }
+    if h2.Lookup(str2) {
+        t.Error("The string should be non-exist.")
+    }
+    if h2.Lookup(str3) {
+        t.Error("The string should be non-exist.")
+    }
 }
