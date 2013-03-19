bdcb748ec3cf36d72a90e171431c2d7c88bd0a77
Brooklyn <yuanotes@gmail.com>
2013-03-19 22:05:31 +0800

    update time complexity of hanoi

--- src/hanoi/hanoi.go
+++ src/hanoi/hanoi.go
@@ -5,6 +5,7 @@ import "fmt"
 func Hanoi(A, B, C byte, n int) {
     // A, B, C for stack 'A', 'B' and 'C'
     // n for the number of plates.
+    // complexity: 2^n -1 = O(2^n)
     if (n==1) {
         fmt.Printf("Move disk %d from %q to %q\n", n, A, C)
     } else {
